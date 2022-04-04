49694b1637b1b97f75fe1a2ef389cc0bf6d2ac99
Chirag Singla <chirag.singla.pi@gmail.com>
2022-04-04 18:22:37 +0530

    :sparkles: feat: add support for transparent images(pngs)

--- colors.js
+++ colors.js
@@ -1,97 +1,97 @@
 const color_map = {
-  0xbe0039: {
+  0xbe0039ff: {
     id: 1,
     name: 'Dark red',
   },
-  0xff4500: {
+  0xff4500ff: {
     id: 2,
     name: 'Bright Red',
   },
-  0xffa800: {
+  0xffa800ff: {
     id: 3,
     name: 'Orange',
   },
-  0xffd635: {
+  0xffd635ff: {
     id: 4,
     name: 'Yellow',
   },
-  0x00a368: {
+  0x00a368ff: {
     id: 6,
     name: 'Dark Green',
   },
-  0x00cc78: {
+  0x00cc78ff: {
     id: 7,
     name: 'Green',
   },
-  0x7eed56: {
+  0x7eed56ff: {
     id: 8,
     name: 'Light Green',
   },
-  0x00756f: {
+  0x00756fff: {
     id: 9,
     name: 'Dark Teal',
   },
-  0x009eaa: {
+  0x009eaaff: {
     id: 10,
     name: 'Teal',
   },
-  0x2450a4: {
+  0x2450a4ff: {
     id: 12,
     name: 'Dark Blue',
   },
-  0x3690ea: {
+  0x3690eaff: {
     id: 13,
     name: 'Blue',
   },
-  0x51e9f4: {
+  0x51e9f4ff: {
     id: 14,
     name: 'Light Blue',
   },
-  0x493ac1: {
+  0x493ac1ff: {
     id: 15,
     name: 'Indigo',
   },
-  0x6a5cff: {
+  0x6a5cffff: {
     id: 16,
     name: 'Periwinkle',
   },
-  0x811e9f: {
+  0x811e9fff: {
     id: 18,
     name: 'Dark Purple',
   },
-  0xb44ac0: {
+  0xb44ac0ff: {
     id: 19,
     name: 'Purple',
   },
-  0xff3881: {
+  0xff3881ff: {
     id: 22,
     name: 'Pink',
   },
-  0xff99aa: {
+  0xff99aaff: {
     id: 23,
     name: 'Light Pink',
   },
-  0x6d482f: {
+  0x6d482fff: {
     id: 24,
     name: 'Dark Brown',
   },
-  0x9c6926: {
+  0x9c6926ff: {
     id: 25,
     name: 'Brown',
   },
-  0x000000: {
+  0x000000ff: {
     id: 27,
     name: 'Black',
   },
-  0x898d90: {
+  0x898d90ff: {
     id: 29,
     name: 'Gray',
   },
-  0xd4d7d9: {
+  0xd4d7d9ff: {
     id: 30,
     name: 'Light Gray',
   },
-  0xffffff: {
+  0xffffffff: {
     id: 31,
     name: 'White',
   },
